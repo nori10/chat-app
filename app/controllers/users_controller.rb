352fa5abc02efda8a6d3471991035d59a112caf4
Norito Yamamura <norito@dotinstall.com>
2020-06-16 12:12:26 +0900

    細かい修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     def user_page
-      @posts = Post.where(user_id: current_user.id)
+      @posts = Post.where(user_id: current_user.id).all.order(created_at: :desc)
     end
   
     def destroy
